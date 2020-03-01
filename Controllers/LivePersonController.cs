220733c51666846f023f586896da4986699b8190
Artur Laksberg <arturl@microsoft.com>
2020-03-01 14:41:35 -0800

    Add LivePerson controller

---
+++ Controllers/LivePersonController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Integration.AspNet.Core;
+
+namespace LPProxyBot.Controllers
+{
+    [Route("api/LivePerson")]
+    [ApiController]
+    public class LivePersonController : ControllerBase
+    {
+        private readonly IBotFrameworkHttpAdapter Adapter;
+        private readonly IBot Bot;
+
+        public LivePersonController(IBotFrameworkHttpAdapter adapter, IBot bot)
+        {
+            Adapter = adapter;
+            Bot = bot;
+        }
+
+        [HttpPost]
+        public async Task PostAsync()
+        {
+            // Delegate the processing of the HTTP POST to the adapter.
+            // The adapter will invoke the bot.
+            var r = Request;
+            Response.StatusCode = 200;
+        }
+    }
+}
