c15de50823ad56be5700f56510240803f4e46a6e
Artur Laksberg <arturl@microsoft.com>
2020-03-01 11:14:35 -0800

    Echo bot template

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild EchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=.\arturl-lpproxy-Web-Deploy.pubxml -p:Password=AMRqHZJP5w8NDKRzN8ztpXClCkc6j6bt79LbiRMENiW3CqwSwHWflRwHQdZG
+
